6447a6fef99e4dbb34d72ed9e38374b2c4b06456
EMPEROR <sahilsorte8457@gmail.com>
2023-10-30 21:28:18 +0530

    Landing Page UI and Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,7 +109,39 @@ export default function Home() {
               </span>
             </div>
           </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-1-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Step 2</span>
+              <span className="text-xl font-semibold">Upload your PDF file</span>
+              <span className="mt-2 text-zinc-700">
+              We&apos;ll process your file and make it ready for you to chat with.
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-1-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Step 3</span>
+              <span className="text-xl font-semibold">Start asking questions</span>
+              <span className="mt-2 text-zinc-700">
+              It&apos;s that simple. Try out PDFAskMate today - it really takes less than a minute.
+              </span>
+            </div>
+          </li>
         </ol>
+        <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+              <div className='mt-16 flow-root sm:mt-24'>
+                <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                  <Image
+                    src='/file-upload-preview.jpg'
+                    alt='upload preview'
+                    width={1419}
+                    height={732}
+                    quality={100}
+                    className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                  />
+                </div>
+              </div>
+            </div>
       </div>
 
     </>
