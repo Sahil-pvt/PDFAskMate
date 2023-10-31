5aa430d6c41c8169f44d55063a0ea476db672f19
EMPEROR <sahilsorte8457@gmail.com>
2023-10-31 23:00:59 +0530

    Created database

---
+++ src/trpc/index.ts
@@ -0,0 +1,34 @@
+import { db } from '@/db';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { TRPCError } from '@trpc/server'
+import { publicProcedure, router } from './trpc';
+
+export const appRouter = router({
+    authCallback: publicProcedure.query(async () => {
+        const { getUser } = getKindeServerSession()
+        const user = getUser()
+
+        if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" })
+
+        //check if the user is in the database
+        const dbUser = await db.user.findFirst({
+            where: {
+                id: user.id
+            }
+        })
+
+        if(!dbUser) {
+            await db.user.create({
+                data: {
+                    id: user.id,
+                    email: user.email
+                }
+            })
+        }
+
+        return {success: true}
+    }),
+})
+
+
+export type AppRouter = typeof appRouter;
