5144c19ec1136ff7ba53fb7a511e45d849bb89ad
EMPEROR <sahilsorte8457@gmail.com>
2023-11-02 17:48:26 +0530

    Feature bar for PDF renderer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import "react-loading-skeleton/dist/skeleton.css"
 import { Toaster } from '@/components/ui/toaster'
+import "simplebar-react/dist/simplebar.min.css"
 
 const inter = Inter({ subsets: ['latin'] })
 
