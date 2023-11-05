d48cb35a850481165873c373b4222284f88f91c7
EMPEROR <sahilsorte8457@gmail.com>
2023-11-05 16:31:17 +0530

    Pricing page and stripe payment integration

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,5 +1,8 @@
 import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
+import { PLANS } from '@/config/stripe';
 import { db } from '@/db';
+import { getUserSubscriptionPlan, stripe } from '@/lib/stripe';
+import { absoluteUrl } from '@/lib/utils';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { TRPCError } from '@trpc/server'
 import { Input } from 'postcss';
@@ -42,15 +45,73 @@ export const appRouter = router({
         })
     }),
 
+    createStripeSession: privateProcedure.mutation(
+        async ({ ctx }) => {
+            const { userId } = ctx
+
+            const billingUrl = absoluteUrl('/dashboard/billing')
+
+            if (!userId)
+                throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+            const dbUser = await db.user.findFirst({
+                where: {
+                    id: userId,
+                },
+            })
+
+            if (!dbUser)
+                throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+            const subscriptionPlan =
+                await getUserSubscriptionPlan()
+
+            if (
+                subscriptionPlan.isSubscribed &&
+                dbUser.stripeCustomerId
+            ) {
+                const stripeSession =
+                    await stripe.billingPortal.sessions.create({
+                        customer: dbUser.stripeCustomerId,
+                        return_url: billingUrl,
+                    })
+
+                return { url: stripeSession.url }
+            }
+
+            const stripeSession =
+                await stripe.checkout.sessions.create({
+                    success_url: billingUrl,
+                    cancel_url: billingUrl,
+                    payment_method_types: ['card'],
+                    mode: 'subscription',
+                    billing_address_collection: 'auto',
+                    line_items: [
+                        {
+                            price: PLANS.find(
+                                (plan) => plan.name === 'Pro'
+                            )?.price.priceIds.test,
+                            quantity: 1,
+                        },
+                    ],
+                    metadata: {
+                        userId: userId,
+                    },
+                })
+
+            return { url: stripeSession.url }
+        }
+    ),
+
     getFileMessages: privateProcedure.input(
         z.object({
             limit: z.number().min(1).max(100).nullish(),
             cursor: z.string().nullish(),
             fileId: z.string()
         })
-    ).query(async ({ctx, input}) => {
-        const {userId} = ctx
-        const {fileId, cursor} = input
+    ).query(async ({ ctx, input }) => {
+        const { userId } = ctx
+        const { fileId, cursor } = input
         const limit = input.limit ?? INFINITE_QUERY_LIMIT
 
         const file = await db.file.findFirst({
@@ -60,7 +121,7 @@ export const appRouter = router({
             }
         })
 
-        if(!file) throw new TRPCError({code: 'NOT_FOUND'})
+        if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
 
         const messages = await db.message.findMany({
             take: limit + 1,
@@ -70,7 +131,7 @@ export const appRouter = router({
             orderBy: {
                 createdAt: "desc"
             },
-            cursor: cursor ? {id: cursor} : undefined,
+            cursor: cursor ? { id: cursor } : undefined,
             select: {
                 id: true,
                 isUserMessage: true,
@@ -81,7 +142,7 @@ export const appRouter = router({
 
         let nextCursor: typeof cursor | undefined = undefined
 
-        if(messages.length > limit) {
+        if (messages.length > limit) {
             const nextItem = messages.pop()
             nextCursor = nextItem?.id
         }
