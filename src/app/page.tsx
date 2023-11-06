787bb25f164cdb479ecbe63204310350967e7ac5
EMPEROR <sahilsorte8457@gmail.com>
2023-11-06 14:58:26 +0530

    Minor changes on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,19 @@ export default function Home() {
     <>
       <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
         <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
-          <p className='text-sm font-semibold text-gray-700'>
-            PDFAskMate is now public!
+          <p className='text-sm font-semibold text-green-600'>
+            Under Development!
           </p>
         </div>
         <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
           Chat with your{' '}
           <span className='text-blue-600'>PDFs</span>{' '}
           in seconds.
+        </h1>
+        <h1 className='max-w-prose mt-5 font-semibold md:text-3xl'>
+          Use{' '}
+          <span className='text-green-600'>Desktop</span>{' '}
+          version for best experience.
         </h1>
         <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
           PDFAskMate allows you to have conversations with any
