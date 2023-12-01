a583b3ed8c3e75e06a74fc98232859e01f4484a6
EMPEROR <sahilsorte8457@gmail.com>
2023-12-01 21:35:38 +0530

    Upload button update

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,67 +1,70 @@
 'use client'
 
-import { trpc } from '@/app/_trpc/client'
-import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent } from '@/components/ui/dialog'
-import { Progress } from '@/components/ui/progress'
-import { useToast } from '@/components/ui/use-toast'
-import { useUploadThing } from '@/lib/uploadthing'
-import { DialogTrigger } from '@radix-ui/react-dialog'
-import { CloudIcon, FileIcon, Loader2Icon } from 'lucide-react'
-import { useRouter } from 'next/navigation'
 import { useState } from 'react'
-import Dropzone, { useDropzone } from 'react-dropzone'
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from './ui/dialog'
+import { Button } from './ui/button'
 
-import { cn } from '@/lib/utils'
+import Dropzone from 'react-dropzone'
+import { Cloud, File, Loader2 } from 'lucide-react'
+import { Progress } from './ui/progress'
+import { useUploadThing } from '@/lib/uploadthing'
+import { useToast } from './ui/use-toast'
+import { trpc } from '@/app/_trpc/client'
+import { useRouter } from 'next/navigation'
 
-const UploadDropzone = ({ isSubscribed }: { isSubscribed: boolean }) => {
-  const [isUploading, setIsUploading] = useState(false)
-  const [uploadProgress, setUploadProgress] = useState(0)
-  const [isHovering, setIsHovering] = useState(false)
-
+const UploadDropzone = ({
+  isSubscribed,
+}: {
+  isSubscribed: boolean
+}) => {
   const router = useRouter()
 
+  const [isUploading, setIsUploading] =
+    useState<boolean>(false)
+  const [uploadProgress, setUploadProgress] =
+    useState<number>(0)
   const { toast } = useToast()
 
   const { startUpload } = useUploadThing(
     isSubscribed ? 'proPlanUploader' : 'freePlanUploader'
   )
 
+  const { mutate: startPolling } = trpc.getFile.useMutation(
+    {
+      onSuccess: (file) => {
+        router.push(`/dashboard/${file.id}`)
+      },
+      retry: true,
+      retryDelay: 500,
+    }
+  )
+
   const startSimulatedProgress = () => {
     setUploadProgress(0)
 
     const interval = setInterval(() => {
-      setUploadProgress((prev) => {
-        if (prev >= 95) {
+      setUploadProgress((prevProgress) => {
+        if (prevProgress >= 95) {
           clearInterval(interval)
-          return prev
+          return prevProgress
         }
-        return prev + 5
+        return prevProgress + 5
       })
     }, 500)
 
     return interval
   }
 
-  const { mutate: startPolling } = trpc.getFile.useMutation({
-    onSuccess: (file) => {
-      router.push(`/dashboard/${file.id}`)
-    },
-    retry: true,
-    retryDelay: 500,
-  })
-
-  const { open } = useDropzone()
-
   return (
     <Dropzone
       multiple={false}
-      onDragEnter={() => setIsHovering(true)}
-      onDragLeave={() => setIsHovering(false)}
-      noClick={true}
       onDrop={async (acceptedFile) => {
-        setIsHovering(false)
         setIsUploading(true)
+
         const progressInterval = startSimulatedProgress()
 
         // handle file uploading
@@ -76,6 +79,7 @@ const UploadDropzone = ({ isSubscribed }: { isSubscribed: boolean }) => {
         }
 
         const [fileResponse] = res
+
         const key = fileResponse?.key
 
         if (!key) {
@@ -86,73 +90,68 @@ const UploadDropzone = ({ isSubscribed }: { isSubscribed: boolean }) => {
           })
         }
 
-        startPolling({ key })
-
         clearInterval(progressInterval)
         setUploadProgress(100)
-      }}
-    >
+
+        startPolling({ key })
+      }}>
       {({ getRootProps, getInputProps, acceptedFiles }) => (
         <div
-          onClick={open}
           {...getRootProps()}
-          className={cn(
-            'm-4 h-64 rounded-lg border border-dashed border-zinc-300 bg-zinc-50 p-4 dark:border-2 dark:border-zinc-800 dark:bg-zinc-900/25',
-            isHovering && 'bg-zinc-100 dark:bg-zinc-900'
-          )}
-        >
-          <div className="flex h-full w-full items-center justify-center">
+          className='border h-64 m-4 border-dashed border-gray-300 rounded-lg'>
+          <div className='flex items-center justify-center h-full w-full'>
             <label
-              htmlFor="dropzone-file"
-              className={cn(
-                'flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-lg'
-              )}
-            >
-              <div className="flex flex-col items-center justify-center pb-6 pt-5">
-                <CloudIcon className="mb-2 h-6 w-6 text-zinc-500" />
-                <p className="mb-2 text-sm text-zinc-700">
-                  <span className="font-semibold">Click to upload</span> or drag
-                  and drop
+              htmlFor='dropzone-file'
+              className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'>
+              <div className='flex flex-col items-center justify-center pt-5 pb-6'>
+                <Cloud className='h-6 w-6 text-zinc-500 mb-2' />
+                <p className='mb-2 text-sm text-zinc-700'>
+                  <span className='font-semibold'>
+                    Click to upload
+                  </span>{' '}
+                  or drag and drop
                 </p>
-                <p className="text-xs text-zinc-500">
-                  PDF (up to {isSubscribed ? '16' : '4'}MB)
+                <p className='text-xs text-zinc-500'>
+                  PDF (up to {isSubscribed ? "16" : "4"}MB)
                 </p>
               </div>
 
-              {acceptedFiles && acceptedFiles.length ? (
-                <div className="flex max-w-xs items-center divide-x divide-zinc-200 overflow-hidden rounded-md bg-white outline outline-[1px] outline-zinc-200 dark:divide-zinc-700 dark:bg-zinc-900 dark:outline-zinc-700">
-                  <div className="grid h-full place-items-center px-3 py-2">
-                    <FileIcon className="h-4 w-4 text-blue-500" />
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className='max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200'>
+                  <div className='px-3 py-2 h-full grid place-items-center'>
+                    <File className='h-4 w-4 text-blue-500' />
                   </div>
-                  <div className="h-full truncate px-3 py-2 text-sm dark:text-zinc-300">
+                  <div className='px-3 py-2 h-full text-sm truncate'>
                     {acceptedFiles[0].name}
                   </div>
                 </div>
               ) : null}
 
               {isUploading ? (
-                <div className="mx-auto mt-4 w-full max-w-xs">
+                <div className='w-full mt-4 max-w-xs mx-auto'>
                   <Progress
                     indicatorColor={
-                      uploadProgress === 100 ? 'bg-green-500' : ''
+                      uploadProgress === 100
+                        ? 'bg-green-500'
+                        : ''
                     }
                     value={uploadProgress}
-                    className="h-1 w-full bg-zinc-200"
+                    className='h-1 w-full bg-zinc-200'
                   />
                   {uploadProgress === 100 ? (
-                    <div className="flex items-center justify-center gap-1 pt-2 text-center text-sm text-zinc-700">
-                      <Loader2Icon className="h-3 w-3 animate-spin" />
-                      Redirecting…
+                    <div className='flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2'>
+                      <Loader2 className='h-3 w-3 animate-spin' />
+                      Redirecting...
                     </div>
                   ) : null}
                 </div>
               ) : null}
 
               <input
-                type="file"
-                className="hidden"
                 {...getInputProps()}
-                id="dropzone-file"
+                type='file'
+                id='dropzone-file'
+                className='hidden'
               />
             </label>
           </div>
@@ -162,21 +161,28 @@ const UploadDropzone = ({ isSubscribed }: { isSubscribed: boolean }) => {
   )
 }
 
-const UploadButton = ({ isSubscribed }: { isSubscribed: boolean }) => {
-  const [isOpen, setIsOpen] = useState(false)
+const UploadButton = ({
+  isSubscribed,
+}: {
+  isSubscribed: boolean
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
-
-      <DialogTrigger asChild>
-        <Button
-          onClick={() => setIsOpen(true)}>
-          Upload PDF
-        </Button>
+    <Dialog
+      open={isOpen}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v)
+        }
+      }}>
+      <DialogTrigger
+        onClick={() => setIsOpen(true)}
+        asChild>
+        <Button>Upload PDF</Button>
       </DialogTrigger>
 
-
-      <DialogContent className="dark:border-neutral-800">
+      <DialogContent>
         <UploadDropzone isSubscribed={isSubscribed} />
       </DialogContent>
     </Dialog>
